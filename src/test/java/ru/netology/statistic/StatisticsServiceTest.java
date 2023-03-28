ed6d6af8b73f16df8930dc2170c86f4573fe2945
Кочнев Илья <Bankir369@gmail.com>
2023-03-28 23:27:32 +0300

    Inicial commit

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -17,4 +17,15 @@ public class StatisticsServiceTest {
 
         Assertions.assertEquals(expected, actual);
     }
+    @Test
+    void findMaxi() {
+        ru.netology.statistic.StatisticsService service = new ru.netology.statistic.StatisticsService();
+
+        long[] incomesInBillions = {12, 5, 8, 4, 15, 3, 15, 6, 11, 11, 12};
+        long expected = 15;
+
+        long actual = service.findMax(incomesInBillions);
+
+        Assertions.assertEquals(expected, actual);
+    }
 }
